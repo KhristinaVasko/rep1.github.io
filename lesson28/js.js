1401d1a8964628399ea0ce1580106bfabb7fb54a
Khristina Vasko <tinavasko0904@gmail.com>
2020-04-26 12:29:26 +0300

    lesson28 task 2(marker classes)

--- lesson28/js.js
+++ lesson28/js.js
@@ -30,4 +30,39 @@ console.log("Діаметр кола = " + circle.diametr);
 circle.calcArea();
 circle.calcLength();
 
+// Реализовать класс, описывающий простой маркер
+
+class Marker {
+  color = 'darkgrey'
+  amountInk = 10
+  printText(){
+      let line1 = prompt("Введіть текст"," ");
+      let letter = 0.5;
+      var n = line1.length;
+      if(n<=this.amountInk*letter){
+          console.log("Рядок = " + line1);
+      }
+      else{
+          console.log(line1 + " Чорнилa не вистачить для цього тексту");
+      }
+  }
+  
+}
+let marker1 = new Marker();
+marker1.printText();
+
+// Реализовать класс, описывающий заправляющийся маркер, унаследовав его от простого маркера и добавив метод для заправки маркера.
+
+class FullMarker extends Marker{
+   amountInk = 100
+  fillMarker(){
+   console.log(`Маркер заправлений на ${this.amountInk} %`);
+  }
+}
+let markerf2 = new FullMarker;
+markerf2.fillMarker();
+
+
+
+
 

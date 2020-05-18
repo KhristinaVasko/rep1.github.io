7ab56202128c914c598f9cdcaf3d2d0196580d3f
Khristina Vasko <tinavasko0904@gmail.com>
2020-05-18 14:08:29 +0300

    exam2 not finished

---
+++ exam2/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: false,
+        autoPlay:true,
+        dots:true,
+        adaptiveHeight: true,
+        autoplay: true,
+        speed:1000,
+        autolaySpeed: 100,
+    }
+    
+    );
+    
+}); 
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}

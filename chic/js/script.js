7dffb1fb34260ca1184e760b42fd93dfbbf51a2b
Khristina Vasko <tinavasko0904@gmail.com>
2020-11-27 02:28:26 +0200

    update header

--- chic/js/script.js
+++ chic/js/script.js
@@ -1,38 +1,5 @@
-$(document).ready(function(){
-    $('.slider').slick({
-        // autoplay: true,
-        nextArrow:"<button  class='slick-next pull-right'>Scroll</button>",
-        dots: true,
+$(function() {
+    $(".header__lang-list-item").on("click", function(e) {
+        $(this).addClass("active").siblings().removeClass("active");
     });
-});
-// modal window
-document.getElementById('button-open').addEventListener('click', 
-function() {
-    document.querySelector('.bg-modal').style.display ='flex';
-    document.querySelector('.slider').style.display ='none';
-   
-});
-document.getElementById('close').addEventListener('click', 
-function() {
-    document.querySelector('.bg-modal').style.display ='none';
-    document.querySelector('.slider').style.display ='block';
-});
-//open modal fron another button
-document.getElementById('button-open1').addEventListener('click', 
-function() {
-    document.querySelector('.bg-modal').style.display ='flex';
-    document.querySelector('.slider').style.display ='block';
-});
-document.getElementById('close').addEventListener('click', 
-function() {
-    document.querySelector('.bg-modal').style.display ='none';
-});
-// burger menu
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+})

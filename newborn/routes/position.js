676ed5d8f7836288a0b71ccb0fc383371130bbbf
Khrystyna_Vasko <khristinavasko0904@gmail.com>
2022-03-26 19:47:05 +0200

    Bachelor's work started, project created, server created + MongoDB connected to the project

---
+++ newborn/routes/position.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const controller = require('../controllers/position')
+const router = express.Router();
+
+router.get('/', controller.getByCategory)
+router.post('/', controller.create)
+router.patch('/:id', controller.update)
+router.delete('/:id', controller.remove)
+
+module.exports = router
+
